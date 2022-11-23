6eadbb3545594abcfc509d5270c07a1685bd2858
eslam abdulmola <eslam.infoedsx@gmail.com>
2022-11-23 10:58:40 +0100

    update

--- src/Client.js
+++ src/Client.js
@@ -1042,7 +1042,6 @@ class Client extends EventEmitter {
         }
 
         return await this.pupPage.evaluate(async number => {
-            const wid = window.Store.WidFactory.createWid(number);
             const result = await window.Store.QueryExist("phone", "+" + number);
             if (!result || result.wid === undefined) return null;
             return result.wid;
